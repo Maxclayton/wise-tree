a8238ff6b379015ff5d1d288063e74a3244690d6
Max Clayton <maxclayton07@gmail.com>
2019-03-07 09:02:10 -0700

    commented out fatPpl

--- swpeople.js
+++ swpeople.js
@@ -3,9 +3,9 @@ import {people} from './people.js';
 const men = people.filter(person => person.gender === 'male');
 const women = people.filter(person => person.gender === 'female');
 const other = people.filter(person => (person.gender === 'n/a') || (person.gender === 'hermaphrodite') || (person.gender === 'none'));
-const shortPpl = people.filter(person => Number(person.height) < 100);
-const fatPpl = people.filter(person => Number(person.mass.replace(/,/g, '')) > 1000);
-console.log(fatPpl);
+//const shortPpl = people.filter(person => Number(person.height) < 100);
+//const fatPpl = people.filter(person => Number(person.mass.replace(/,/g, '')) > 1000);
+//console.log(fatPpl);
 //console.log(men, women, other)
 
 const mainContainer = document.querySelector('.man-holder');
@@ -32,3 +32,16 @@ women.forEach((man) => {
     manElement.appendChild(eyeColor);
     womanContainer.appendChild(manElement);
 })
+
+//const fatPplContainer = document.querySelector('.fatPplContainer');
+
+
+//man.forEach((man) => {
+  //  let manElement = document.createElement('div');
+    //manElement.className = 'box';
+    //manElement.textContent = man.name;
+    //let mass = document.createElement('p');
+    //mass.textContent = man.mass;
+    //manElement.appendChild(mass);
+    //womanContainer.appendChild(manElement);
+//})
