2e64928ce582e141b2ee76b3d80662f6612e501f
Max Clayton <maxclayton07@gmail.com>
2019-03-13 15:56:14 -0600

    Boxes

--- swpeople.js
+++ swpeople.js
@@ -1,12 +1,12 @@
 import {people} from './people.js';
 
-const men = people.filter(person => person.gender === 'male');
-const women = people.filter(person => person.gender === 'female');
-const other = people.filter(person => (person.gender === 'n/a') || (person.gender === 'hermaphrodite') || (person.gender === 'none'));
+//const men = people.filter(person => person.gender === 'male');
+//const women = people.filter(person => person.gender === 'female');
+//const other = people.filter(person => (person.gender === 'n/a') || (person.gender === 'hermaphrodite') || (person.gender === 'none'));
 //const shortPpl = people.filter(person => Number(person.height) < 100);
-//const fatPpl = people.filter(person => Number(person.mass.replace(/,/g, '')) > 1000);
-//console.log(fatPpl);
-//console.log(men, women, other)
+const fatPpl = people.filter(person => Number(person.mass.replace(/,/g, '')) > 1000);
+console.log(fatPpl);
+console.log(men, women, other)
 
 const mainContainer = document.querySelector('.man-holder');
 men.forEach((man) => {
@@ -33,15 +33,15 @@ women.forEach((man) => {
     womanContainer.appendChild(manElement);
 })
 
-//const fatPplContainer = document.querySelector('.fatPplContainer');
+const fatPplContainer = document.querySelector('.fatPplContainer');
 
 
-//man.forEach((man) => {
-  //  let manElement = document.createElement('div');
-    //manElement.className = 'box';
-    //manElement.textContent = man.name;
-    //let mass = document.createElement('p');
-    //mass.textContent = man.mass;
-    //manElement.appendChild(mass);
-    //womanContainer.appendChild(manElement);
-//})
+man.forEach((man) => {
+    let manElement = document.createElement('div');
+    manElement.className = 'box';
+    manElement.textContent = man.name;
+    let mass = document.createElement('p');
+    mass.textContent = man.mass;
+    manElement.appendChild(mass);
+    womanContainer.appendChild(manElement);
+})
