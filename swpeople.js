79c96ba9035b4583631dcca48167eb9786b61f39
Max Clayton <maxclayton07@gmail.com>
2019-03-13 19:30:00 -0600

    changed to mass

--- swpeople.js
+++ swpeople.js
@@ -1,47 +1,34 @@
 import {people} from './people.js';
 
-//const men = people.filter(person => person.gender === 'male');
-//const women = people.filter(person => person.gender === 'female');
-//const other = people.filter(person => (person.gender === 'n/a') || (person.gender === 'hermaphrodite') || (person.gender === 'none'));
-//const shortPpl = people.filter(person => Number(person.height) < 100);
-const fatPpl = people.filter(person => Number(person.mass.replace(/,/g, '')) > 1000);
+
+const fatPpl = people.filter(person => Number(person.mass.replace(/,/g, '')) > 100);
 console.log(fatPpl);
-console.log(men, women, other)
-
-const mainContainer = document.querySelector('.man-holder');
-men.forEach((man) => {
-    let manElement = document.createElement('div');
-    manElement.className = 'box';
-    manElement.textContent = man.name;
-    let eyeColor = document.createElement('p');
-    eyeColor.textContent = man.eye_color;
-    manElement.appendChild(eyeColor);
-    mainContainer.appendChild(manElement);
-});
+const skinnyPpl = people.filter(person => Number(person.mass.replace(/,/g, '')) < 50);
+console.log(skinnyPpl);
+//console.log(men, women, other)
 
 
-const womanContainer = document.querySelector('.woman-holder');
-
-
-women.forEach((man) => {
-    let manElement = document.createElement('div');
-    manElement.className = 'box';
-    manElement.textContent = man.name;
-    let eyeColor = document.createElement('p');
-    eyeColor.textContent = man.eye_color;
-    manElement.appendChild(eyeColor);
-    womanContainer.appendChild(manElement);
-})
 
 const fatPplContainer = document.querySelector('.fatPplContainer');
 
-
-man.forEach((man) => {
-    let manElement = document.createElement('div');
-    manElement.className = 'box';
-    manElement.textContent = man.name;
+fatPpl.forEach((fatPerson) => {
+    let fatPersonElement = document.createElement('div');
+    fatPersonElement.className = 'box';
+    fatPersonElement.textContent = fatPerson.name;
     let mass = document.createElement('p');
-    mass.textContent = man.mass;
-    manElement.appendChild(mass);
-    womanContainer.appendChild(manElement);
+    mass.textContent = fatPerson.mass;
+    fatPersonElement.appendChild(mass);
+    fatPplContainer.appendChild(fatPersonElement);
+})
+
+const skinnyPplContainer = document.querySelector('.skinnyPplContainer');
+
+skinnyPpl.forEach((skinnyPerson) => {
+    let skinnyPersonElement = document.createElement('div');
+    skinnyPersonElement.className = 'box';
+    skinnyPersonElement.textContent = skinnyPerson.name;
+    let mass = document.createElement('p') ;
+    mass.textContent = skinnyPerson.mass;
+    skinnyPersonElement.appendChild(mass);
+    skinnyPplContainer.appendChild(skinnyPersonElement);
 })
