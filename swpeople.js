e0145309d1299f1fb47d9060c49f122db1fcbd2d
Max Clayton <maxclayton07@gmail.com>
2019-03-27 14:44:05 -0600

    started pokemon cards

--- swpeople.js
+++ swpeople.js
@@ -1,42 +1,47 @@
-import {people} from './people.js';
+import { people } from "./people.js";
 
-
-const fatPpl = people.filter(person => Number(person.mass.replace(/,/g, '')) > 100); 
-fatPpl.sort(function (a,b) {
-    return Number(b.mass.replace(/,/g, '')) - Number(a.mass.replace(/,/g, ''));
- });
+const fatPpl = people.filter(
+  person => Number(person.mass.replace(/,/g, "")) > 100
+);
+fatPpl.sort(function(a, b) {
+  return Number(b.mass.replace(/,/g, "")) - Number(a.mass.replace(/,/g, ""));
+});
 console.log(fatPpl);
-const skinnyPpl = people.filter(person => Number(person.mass.replace(/,/g, '')) < 50);
-skinnyPpl.sort(function (a,b) {
-    return Number(b.mass.replace(/,/g, '')) - Number(a.mass.replace(/,/g, ''));
+const skinnyPpl = people.filter(
+  person => Number(person.mass.replace(/,/g, "")) < 50
+);
+skinnyPpl.sort(function(a, b) {
+  return Number(b.mass.replace(/,/g, "")) - Number(a.mass.replace(/,/g, ""));
 });
 console.log(skinnyPpl);
 //console.log(men, women, other)
 
+const fatPplContainer = document.querySelector(".fatPplContainer");
 
-
-const fatPplContainer = document.querySelector('.fatPplContainer');
-
- fatPpl.forEach((fatPerson) => {
-    let fatPersonElement = document.createElement('div');
-    fatPersonElement.className = 'box';
-    fatPersonElement.textContent = fatPerson.name;
-    let mass = document.createElement('p');
-    mass.textContent = fatPerson.mass;
-    fatPersonElement.appendChild(mass);
-    fatPplContainer.appendChild(fatPersonElement);
-     { console.log(fatPpl);}
+fatPpl.forEach(fatPerson => {
+  let fatPersonElement = document.createElement("div");
+  fatPersonElement.className = "box";
+  fatPersonElement.textContent = fatPerson.name;
+  let mass = document.createElement("p");
+  mass.textContent = fatPerson.mass;
+  fatPersonElement.appendChild(mass);
+  fatPplContainer.appendChild(fatPersonElement);
+  {
+    console.log(fatPpl);
+  }
 });
 
-const skinnyPplContainer = document.querySelector('.skinnyPplContainer');
+const skinnyPplContainer = document.querySelector(".skinnyPplContainer");
 
-skinnyPpl.forEach((skinnyPerson) => {
-    let skinnyPersonElement = document.createElement('div');
-    skinnyPersonElement.className = 'box';
-    skinnyPersonElement.textContent = skinnyPerson.name;
-    let mass = document.createElement('p') ;
-    mass.textContent = skinnyPerson.mass;
-    skinnyPersonElement.appendChild(mass);
-    skinnyPplContainer.appendChild(skinnyPersonElement);
-    { console.log(skinnyPpl);}
+skinnyPpl.forEach(skinnyPerson => {
+  let skinnyPersonElement = document.createElement("div");
+  skinnyPersonElement.className = "box";
+  skinnyPersonElement.textContent = skinnyPerson.name;
+  let mass = document.createElement("p");
+  mass.textContent = skinnyPerson.mass;
+  skinnyPersonElement.appendChild(mass);
+  skinnyPplContainer.appendChild(skinnyPersonElement);
+  {
+    console.log(skinnyPpl);
+  }
 });
