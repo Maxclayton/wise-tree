046950a551083306c3128d7ebac774d8e21054ef
Max Clayton <maxclayton07@gmail.com>
2019-04-03 21:53:34 -0600

    added scripts

---
+++ pokecards.js
@@ -0,0 +1,30 @@
+import { pokemon } from '../data/pokemon.js'
+
+pokemon.forEach((singleMon) => {
+    fetch(singleMon.url)
+    .then(function(response) {
+      return response.json()
+    })
+    .then(function(myJson) {
+      console.log(myJson)
+      createPokeCard(myJson)
+    })
+})
+
+
+
+console.log(pokemon)
+
+const mainContainer = document.querySelector('.container')
+
+function createPokeCard(pokedata) {
+    let card = document.createElement('div')
+    let name = document.createElement('p')
+    let image = document.createElement('img')
+
+    name.textContent = pokedata.name
+    image.src = pokedata.sprites.front_default
+    card.appendChild(name)
+    card.appendChild(image)
+    mainContainer.appendChild(card)
+}
