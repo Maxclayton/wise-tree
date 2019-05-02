185fdd4919dd2753775c2bc50d44b4c8349d10b2
Max Clayton <maxclayton07@gmail.com>
2019-05-01 20:51:30 -0600

    finished

--- js/index.js
+++ js/index.js
@@ -1,17 +1,17 @@
-const { prisma } = require('./generated/prisma-client')
+// //const { prisma } = require('./generated/prisma-client')
 
-// A `main` function so that we can use async/await
-async function main() {
+// // A `main` function so that we can use async/await
+// async function main() {
 
-  // Create a new Pokemon called `Bulbasaur`
-    const newPokemon = await prisma.createPokemon({
-        name: 'Bulbasaur',
-        url: 'http://hp-api.herokuapp.com/api/characters '})
-  console.log(`Created new pokemon: ${newPokemon.name} (ID: ${newPokemon.id})`)
+//   // Create a new Pokemon called `Bulbasaur`
+//     const newPokemon = await prisma.createPokemon({
+//         name: 'Bulbasaur',
+//         url: 'http://hp-api.herokuapp.com/api/characters '})
+//   console.log(`Created new pokemon: ${newPokemon.name} (ID: ${newPokemon.id})`)
 
-  // Read all pokemon from the database and print them to the console
-  const allPokemon = await prisma.pokemons()
-  console.log(allPokemon)
-}
+//   // Read all pokemon from the database and print them to the console
+//   const allPokemon = await prisma.pokemons()
+//   console.log(allPokemon)
+// }
 
-main().catch(e => console.error(e))
+// main().catch(e => console.error(e))
